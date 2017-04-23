c553397a6433ce3447e6efc8c38d1c33ebb3f9df
Riboshom <riboshom@hotmail.com>
2017-04-22 23:26:33 -0400

    Concat formatted channel data per row in test_wrapper

--- examples/test_wrapper.js
+++ examples/test_wrapper.js
@@ -39,12 +39,17 @@ fs.readFile('alf2_zalza_edit.xm', function(err, data) {
 			const pattern = module.get_order_pattern(order);
 			for(var row = 0; row < module.get_pattern_num_rows(pattern); row++) {
 				module.set_position_order_row(order, row);
+				var channelUpdates = [];
 				for(var channel = 0; channel < module.num_channels; channel++) {
-					console.log(module.get_pattern_row_channel(pattern, row, channel).string)
+					channelUpdates.push(module.get_pattern_row_channel(pattern, row, channel))
 				}
+				var rowString = "";
+				channelUpdates.forEach(function(update, idx){
+					if(idx !== 0) rowString += " | ";
+					rowString += update.string;
+				})
+				console.log(rowString)
 			}
-			// FIXME : Break after first one
-			break;
 		}
     }
 });
